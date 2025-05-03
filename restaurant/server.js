d7c6c274899ebf71a2247435261de4aaa2c07dfb
Medy-Daniel <medy.vilgenis@gmail.com>
2025-05-03 12:37:48 +0200

    commit for pm2

--- restaurant/server.js
+++ restaurant/server.js
@@ -158,7 +158,7 @@ app.get('/api/categories', (req, res) => {
 });
 
 // Lancement du serveur
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
 });
